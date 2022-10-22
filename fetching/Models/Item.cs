a6003569e788115a99b151da0a4faecdf030f81d
Aliaksandr Khrystsenka <alex.kr1stehko@gmail.com>
2022-10-22 12:57:57 +0300

    upd: model change

--- fetching/Models/Item.cs
+++ fetching/Models/Item.cs
@@ -7,6 +7,17 @@ public class Item
     public string Name3 { get; set; } = String.Empty;
     public string Price { get; set; } = String.Empty;
     public string Discount { get; set; } = String.Empty;
+    public string Country { get; set; } = String.Empty;
+    public string Producer { get; set; } = String.Empty;
+    public int VendorCode { get; set; } = 0;
+    public float Wieght { get; set; } = 0;
+    public string WieghtUnit { get; set; } = String.Empty;
+    public string Compound { get; set; } = String.Empty;
+    public float Protein { get; set; } = 0;
+    public float Fat { get; set; } = 0;
+    public float Carbo { get; set; } = 0;
+    public float Portion { get; set; } = 0;
+    public List<string> Categories { get; private set; } = new List<string>();
     public string Link { get; set; } = String.Empty;
 
     public override bool Equals(object? obj)
